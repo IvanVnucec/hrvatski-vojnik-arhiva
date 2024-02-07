e63fd45a3b85fdde586c983f8eb02b55da98d3ad
Ivan Vnucec <ivan.vnucec@artronic.net>
2024-02-07 11:20:36 +0100

    add main function

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-
-# run the scraper
-process = CrawlerProcess(get_project_settings())
-process.crawl("hrvatski_vojnik")
-process.start()
-process.join()
-
 def exec(cmd):
     from os import system
     print(cmd)
     system(cmd)
 
-# commit the scraped files
-exec('git config --global user.email "you@example.com"')
-exec('git config --global user.name "GitHub Actions"')
-exec("git add arhiva")
-exec("git commit -m 'Dodaj novi broj Hrvatskog vojnika'")
-exec("git push")
+def main():
+    from scrapy.crawler import CrawlerProcess
+    from scrapy.utils.project import get_project_settings
+    # run the scraper
+    process = CrawlerProcess(get_project_settings())
+    process.crawl("hrvatski_vojnik")
+    process.start()
+    process.join()
+
+    # commit the scraped files
+    exec('git config --global user.email "you@example.com"')
+    exec('git config --global user.name "GitHub Actions"')
+    exec("git add arhiva")
+    exec("git commit -m 'Dodaj novi broj Hrvatskog vojnika'")
+    exec("git push")
+
+if __name__ == "__main__":
+    main()
