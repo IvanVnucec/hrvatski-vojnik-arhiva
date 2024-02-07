bf9d900fdc35ab2b1f6856486adf4297bebd2a2a
Ivan Vnucec <ivan.vnucec@artronic.net>
2024-02-07 08:15:42 +0100

    better filtering

--- scraper/spiders/hrvatski_vojnik_spider.py
+++ scraper/spiders/hrvatski_vojnik_spider.py
@@ -1,18 +1,20 @@
 import scrapy
-
+from scrapy.linkextractors import LinkExtractor
 
 class HrvatskiVojnikSpider(scrapy.Spider):
     name = "hrvatski_vojnik"
     allowed_domains = ["hrvatski-vojnik.hr"]
     start_urls = ["https://hrvatski-vojnik.hr/kategorija/magazin/"]
+    articles_link_extractor = LinkExtractor(allow=r"/kategorija/magazin/", restrict_css="h3.article-preview__title a")
 
     def parse(self, response):
-        title_hrefs = response.css("h3.article-preview__title a::attr(href)").getall()
-        filtered = filter(lambda href: href.startswith("https://hrvatski-vojnik.hr/kategorija/magazin/"), title_hrefs)
-        yield from response.follow_all(filtered, self.parse_article)
+        # filter articles
+        article_urls = self.articles_link_extractor.extract_links(response)
+        yield from response.follow_all(article_urls, callback=self.parse_articles)
+        # next page
         yield from response.follow_all(css="a.next", callback=self.parse)
 
-    def parse_article(self, response):
+    def parse_articles(self, response):
         yield {
-            "file_urls": response.css("a.btn--download-magazin::attr(href)").get(),
+            "file_url": response.css("a.btn--download-magazin::attr(href)").get(),
         }
